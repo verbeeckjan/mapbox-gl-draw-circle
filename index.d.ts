76f63b599d41085d04c7146864f6e2a3157341a3
Anastasiia Tatuian (external) <anastasiia_tatuian@swissre.com>
2021-11-11 00:56:24 +0200

    added types

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module 'mapbox-gl-draw-rounded-circle' {
+    export const CircleMode: object;
+    export const DragCircleMode: object;
+    export const DirectMode: object;
+    export const SimpleSelectMode: object;
+}
